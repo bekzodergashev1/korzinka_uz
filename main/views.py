15dbca7ad4390e54a0b1adf6442677178cc09518
bekzodergashev1 <bekzodergashev138gmail.com>
2023-02-13 22:03:46 +0500

    login

--- main/views.py
+++ main/views.py
@@ -1,10 +1,62 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
 from main.models import *
-from .form import OrderForm
+from .form import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Account was created for " + user)
+
+                return redirect('login')
+
+        context = {"form":form}
+        return render(request, 'main/register.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.info(request, "Username or password incorrect")
+
+
+        context = {}
+        return render(request, 'main/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -15,20 +67,20 @@ def home(request):
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
 
-    context = {'orders':orders, 'customers':customers,
-               'total_customers':total_customers, 'delivered':delivered,
-               'total_orders':total_orders, 'pending':pending}
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'delivered': delivered,
+               'total_orders': total_orders, 'pending': pending}
 
-    return render(request, 'home.html', context)
+    return render(request, 'main/home.html', context)
 
-
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
 
-    context = {'products' : products}
-    return render(request, 'products.html', context)
+    context = {'products': products}
+    return render(request, 'main/products.html', context)
 
-
+@login_required(login_url='login')
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
 
@@ -38,16 +90,15 @@ def customer(request, pk_test):
     myFilter = OrderFilter(request.GET, queryset=orders)
     orders = myFilter.qs
 
+    context = {'customer': customer, 'orders': orders,
+               'order_count': order_count, 'myFilter': myFilter}
+    return render(request, 'main/customer.html', context)
 
-    context = {'customer' : customer, 'orders' : orders,
-               'order_count':order_count, 'myFilter':myFilter}
-    return render(request, 'customer.html', context)
-
-
+@login_required(login_url='login')
 def status(request):
-    return render(request, 'status.html')
+    return render(request, 'main/status.html')
 
-
+@login_required(login_url='login')
 def createOrder(request, pk):
     form = OrderForm()
 
@@ -58,11 +109,10 @@ def createOrder(request, pk):
             return redirect('/')
 
     context = {'form': form}
-    return render(request, 'order_form.html', context)
+    return render(request, 'main/order_form.html', context)
 
-
+@login_required(login_url='login')
 def updateOrder(request, pk):
-
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
 
@@ -72,15 +122,15 @@ def updateOrder(request, pk):
             form.save()
             return redirect('/')
 
-    context = {'form':form}
-    return render(request, 'order_form.html', context)
+    context = {'form': form}
+    return render(request, 'main/order_form.html', context)
 
-
+@login_required(login_url='login')
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
         order.delete()
         return redirect('/')
 
-    context = {'item':order}
-    return render(request, 'delete.html', context)
+    context = {'item': order}
+    return render(request, 'main/delete.html', context)
